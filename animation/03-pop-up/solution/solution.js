98d78cacb451c3d008be53aa3bfe823c7da0c71e
conor <conorjm89@gmail.com>
2021-10-20 21:40:44 +0100

    Set up opacity transition

--- animation/03-pop-up/solution/solution.js
+++ animation/03-pop-up/solution/solution.js
@@ -1,14 +1,18 @@
-const openButton = document.getElementById('open-modal');
+const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
 
 function openModal() {
+  const container = document.querySelector('.popup-container');
   const modalDiv = document.querySelector('.popup-modal');
   modalDiv.classList.add('show');
+  container.classList.add('show');
 }
 
 function closeModal() {
+  const container = document.querySelector('.popup-container.show');
   const modalDiv = document.querySelector('.popup-modal.show');
   modalDiv.classList.remove('show');
+  container.classList.remove('show');
 }
 
 openButton.addEventListener('click', openModal);
