39ed908683b36a3aafda52b9b72b9593a52aa0f1
Andrew Dutson <Dutsandrew@gmail.com>
2023-03-26 10:49:18 -0600

    removed duplicate element selector & added semicolon

--- animation/02-pop-up/script.js
+++ animation/02-pop-up/script.js
@@ -1,10 +1,9 @@
 const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
-const backdrop = document.getElementById('backdrop')
 
 function toggleModal() {
   const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop')
+  const backdrop = document.querySelector('.backdrop');
   modalDiv.classList.toggle('show');
   backdrop.classList.toggle('show');
 }
