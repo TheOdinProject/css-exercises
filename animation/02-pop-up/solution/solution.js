11afd0873a1cc2e977ba882f65d349979cb7e0f5
tusharravenclaw <tushar68053@gmail.com>
2023-10-02 23:03:46 +0530

    update pop-up solution

--- animation/02-pop-up/solution/solution.js
+++ animation/02-pop-up/solution/solution.js
@@ -1,13 +1,12 @@
-const openButton = document.getElementById('trigger-modal');
-const closeButton = document.getElementById('close-modal');
-const backdrop = document.getElementById('backdrop')
+const openButton = document.getElementById("trigger-modal");
+const closeButton = document.getElementById("close-modal");
 
 function toggleModal() {
-  const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop')
-  modalDiv.classList.toggle('show');
-  backdrop.classList.toggle('show');
+  const modalDiv = document.querySelector(".popup-modal");
+  const backdrop = document.querySelector(".backdrop");
+  modalDiv.classList.toggle("show");
+  backdrop.classList.toggle("show");
 }
 
-openButton.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
+openButton.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
