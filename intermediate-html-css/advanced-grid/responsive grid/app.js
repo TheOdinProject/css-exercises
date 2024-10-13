bf0b94b1365c7de4981d1250a75db2d967a1da83
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2024-10-13 22:41:53 +0530

    Add files via upload

---
+++ intermediate-html-css/advanced-grid/responsive grid/app.js
@@ -0,0 +1,19 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+
+    // Burger Animation
+    burger.classList.toggle('toggle');
+});
